ab25c42291ec88f1abb281c55deb0082f3b1be51
kid-116 <mehultodi116@gmail.com>
2021-01-16 12:34:35 +0530

    demo_app created

--- session1/demo_app/ios/Flutter/flutter_export_environment.sh
+++ session1/demo_app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\GitRepos\IRIS-Flutter-Bootcamp-2021\demo_app"
+export "FLUTTER_APPLICATION_PATH=D:\GitRepos\IRIS-Flutter-Bootcamp-2021\session1\demo_app"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
